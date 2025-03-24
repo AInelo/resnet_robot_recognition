934b91f089e9431961df3e5f65e0fd8e7090a0b2
TOTON Lionel <totonlionel@gmail.com>
2025-03-24 19:01:44 +0100

    Send last recognition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from recognition import detect_cube_accurate
+from recognition import detect_cube_accurate, CubeClassifier, VideoCamera
 from movement import move_arm_to_cube, move_to_yellow_place, move_to_green_place, move_to_red_place, move_to_blue_place, take_cube, initialize_arm, look_at_platform
 
 import sys
@@ -10,37 +10,73 @@ Arm_Lib = importlib.import_module('Arm_Lib')
 Arm_Device = Arm_Lib.Arm_Device
 arm = Arm_Device()
 
+# def main():
+#     arm.Arm_serial_set_torque(1)
+
+#     # Initialisation du bras robotisé
+#     initialize_arm()
+#     look_at_platform()
+
+#     # Boucle pour détecter et déplacer les cubes
+#     while True:
+#         # Retour à la position d'initialisation et regarder la plateforme avant chaque capture
+#         initialize_arm()
+#         look_at_platform()
+
+#         # Détection du cube et prédiction de la couleur
+#         predicted_class = detect_cube_accurate()
+
+#         # Affichage du résultat dans la console
+#         if predicted_class:
+#             print(f"Cube détecté: {predicted_class}")
+#         else:
+#             print("Aucun cube détecté")
+
+#         # Si un cube est détecté, il faut le déplacer
+#         if predicted_class:
+#             # Déplacement vers le cube
+#             move_arm_to_cube(cube_position=(320, 240))  # Position hypothétique du cube au centre de l'image
+
+#             # Prendre le cube
+#             take_cube()
+
+#             # Déplacer le cube au bon endroit en fonction de sa couleur
+#             if predicted_class == 'cube_red':
+#                 move_to_red_place()
+#             elif predicted_class == 'cube_yellow':
+#                 move_to_yellow_place()
+#             elif predicted_class == 'cube_green':
+#                 move_to_green_place()
+#             elif predicted_class == 'cube_blue':
+#                 move_to_blue_place()
+
+#             # Attente avant de passer au prochain cube
+#             time.sleep(2)
+
 def main():
     arm.Arm_serial_set_torque(1)
+
+    # Charger le modèle une seule fois
+    model_path = './test-save/resnet_model_best.pth'
+    classifier = CubeClassifier(model_path=model_path)
+    camera = VideoCamera()
 
     # Initialisation du bras robotisé
     initialize_arm()
     look_at_platform()
 
-    # Boucle pour détecter et déplacer les cubes
     while True:
-        # Retour à la position d'initialisation et regarder la plateforme avant chaque capture
         initialize_arm()
         look_at_platform()
 
-        # Détection du cube et prédiction de la couleur
-        predicted_class = detect_cube_accurate()
+        predicted_class = detect_cube_accurate(classifier, camera)
 
-        # Affichage du résultat dans la console
         if predicted_class:
             print(f"Cube détecté: {predicted_class}")
-        else:
-            print("Aucun cube détecté")
 
-        # Si un cube est détecté, il faut le déplacer
-        if predicted_class:
-            # Déplacement vers le cube
-            move_arm_to_cube(cube_position=(320, 240))  # Position hypothétique du cube au centre de l'image
-
-            # Prendre le cube
+            move_arm_to_cube(cube_position=(320, 240))  
             take_cube()
 
-            # Déplacer le cube au bon endroit en fonction de sa couleur
             if predicted_class == 'cube_red':
                 move_to_red_place()
             elif predicted_class == 'cube_yellow':
@@ -50,8 +86,9 @@ def main():
             elif predicted_class == 'cube_blue':
                 move_to_blue_place()
 
-            # Attente avant de passer au prochain cube
             time.sleep(2)
+
+
 
 if __name__ == "__main__":
     main()
