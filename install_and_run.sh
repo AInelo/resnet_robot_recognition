eca7cd87b2cb3b62a103cd7a8d5a34291cd8c425
Lionel JetsonRobot <totonlionel@gmail.com>
2025-03-24 23:37:38 +0800

    Sending last code

--- install_and_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Installer les dépendances à partir du fichier requirements.txt
-echo "Installation des dépendances..."
-pip install -r requirements.txt
-
-# Exécuter le script Python
-echo "Exécution de main.py..."
-python main.py
